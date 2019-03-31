3dec093458f8e461665d4c775396c1b4c2898799
rebenely <venrayyle@gmail.com>
2019-03-31 11:22:15 +0800

    wokring on player view

--- src/views/PlayerView.js
+++ src/views/PlayerView.js
@@ -9,7 +9,40 @@ module.exports = {
   view: function() {
     return m("div", {class: "row"}, [
       m("div.col-sm-6", [
-        m("h1", Player.current.username)
+        m("div", {class: "card fluid"}, [
+          m("div.section", [
+            m("h1", Player.current.username)
+          ]),
+          m("div.section", [
+            m("div.row", [
+              m("div.col-sm-3", [
+                  m("p", "Completion : 69%"),
+                  m("p", "Assistance Usage: 87%"),
+              ]),
+              m("div.col-sm-3", [
+                  m("p", "Sessions : 420"),
+                  m("p", "Time Utilization: 69%"),
+              ]),
+
+              m("div.col-sm-3", [
+                  m("p", "Review Count: 2"),
+                  m("p", "Overall Dungeon Accuracy: 69%"),
+              ]),
+
+              m("div.col-sm-3", [
+                  m("p", "Unskipped Items: 87%"),
+                  m("p[style=color: green;]", "SRL Score: 6969"),
+              ]),
+
+            ]),
+            m("div.row", [
+
+
+
+            ]),
+
+          ])
+        ])
       ]),
       m("div.col-sm-6", [
         m("h1", Player.current.username)
